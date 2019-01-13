dee62913b5c3afb3ceaf53cb868ba830a066095e
Sylwester Gesterom Mazanek <gesterom@gmail.com>
2019-01-13 16:31:36 +0100

    Pawn and Bord

---
+++ include/Board.h
@@ -0,0 +1,19 @@
+#include <array>
+
+
+const int BORDHIGHT = 16;
+const int BORDWIGHT = 16;
+
+using AttacResualt = bool;
+using MoveResualt = bool;
+using Fild=int;
+
+using FildPosition = std::pair<int,int>;
+
+class Board
+{
+	std::array<Fild,BORDHIGHT * BORDWIGHT> board;
+	public:
+	AttacResualt attac(FildPosition attacer,FildPosition defender);
+	MoveResualt move(FildPosition from,FildPosition to);
+};
