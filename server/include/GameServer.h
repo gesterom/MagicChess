c65e5a12398e4c9acd8cf3d78228c6f5e4eb6cad
Sylwester Gesterom Mazanek <gesterom@gmail.com>
2019-04-04 04:02:41 +0200

    suport section in config ini

--- server/include/GameServer.h
+++ server/include/GameServer.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "Config.h"
 
+#include "interface.h" // FIXME remove it too much dependency 
+
 class Config;
 class IRepositoryFactory;
 
 class GameServer
 {
+	int hight,wight;
+	std::vector<ID::Entity::Fild> board;
 	public:
-		GameServer(const Config& config, IRepositoryFactory repo);
+		GameServer(const Config& config, const IRepositoryFactory& repo);
 		void run();
 };
--- server/src/Config.cpp
+++ server/src/Config.cpp
@@ -5,8 +5,19 @@
 std::string removeWhiteSpace(std::string str)
 {
 	std::string res;
+	char last ='\0';
 	for(auto i : str)
 	{
+		if(last == '\\'){
+			if(i == '\\') res+='\\';
+			else if(i == ' ') res+=' ';
+			last = '\0';
+			continue;
+		}
+		if(i == '\\') {
+			last='\\';
+			continue;
+		}
 		if(isspace(i)) continue;
 		else res+=i;
 	}
@@ -16,13 +27,32 @@ std::string removeWhiteSpace(std::string str)
 Config::Config(std::string filename)
 {
 	std::ifstream in(filename);
+	std::string section_name ="";
 	while(in.good())
 	{
+		std::string str;
+		getline(in,str);
+		str = removeWhiteSpace(str);
 		std::string key,value;
-		getline(in,key,'=');
-		getline(in,value);		
-		this->map[removeWhiteSpace(key)] = removeWhiteSpace(value);
-	}	
+		if(str[0] == '[' and str[str.size()-1] == ']')  // section detected 
+		{
+			section_name = str;
+			continue;
+		}
+		else{
+			auto pos = str.find("=");
+			if(pos == std::string::npos) continue;
+			key = str.substr(0, pos);
+			value = str.substr(pos+1);
+		}
+		
+		if(key[0] == ';') continue;
+		this->map[section_name+((section_name.empty())?"":".")+key] = value;
+	}
+	for (auto i : map)
+	{
+		std::cerr<<i.first<<" = "<<i.second<<std::endl;
+	}
 }
 
 template<>
@@ -32,7 +62,7 @@ std::string Config::get(std::string key) const {
 		res = this->map.at(key);
 	}catch(std::out_of_range ex)
 	{
-		//TODO
+		//TODO add loger remove iostream
 		std::cerr<<"Exception : std::out_of_range : "<<__FILE__<<":"<<__PRETTY_FUNCTION__<<"( "<<__LINE__<<" )"<<" Description : Acess to out of rage with key :"<<key<<std::endl;
 	}
 	return res;
