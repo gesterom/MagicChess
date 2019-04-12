791fbbda0495dff12d74751c0b6cc84ab0a2dc49
gesterom <gesterom@gmail.com>
2019-04-12 21:29:28 +0000

    Update Match.h

--- server/include/Match.h
+++ server/include/Match.h
@@ -2,13 +2,13 @@
 
 #include "interface.h"
 #include "Player.h"
-
+#include "Board.h"
 
 class Match{
 	private:
 		Player PlayerW;
 		Player PlayerB;
-		//Board board;
+		Board board;
 	public:
 		Match(ID::Entity::Player pW,ID::Entity::Deck dW,ID::Entity::Player pB,ID::Entity::Deck dB);
 		const Player getPlayer(int playerIdinMtch) const;
